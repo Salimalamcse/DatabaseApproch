c6b885a2f7cb263cfef3e73257b5d5ec1229df80
Salimalamcse <salimalamcse@gmail.com>
2025-02-01 00:14:33 +0530

    Add project files.

---
+++ DatabaseApproch/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseApproch.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Designation { get; set; } = null!;
+
+    public int Salary { get; set; }
+
+    public string City { get; set; } = null!;
+}
